dd6803b560b1b5b273e652a029de40d10552c436
glpshchn <464976@niuitmo.ru>
2024-12-21 11:31:02 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,10 @@
-
 def area(a):
+    if a < 0:
+        raise ValueError("The side of the square cannot be negative.")
     return a * a
 
 
 def perimeter(a):
+    if a < 0:
+        raise ValueError("The side of the square cannot be negative.")
     return 4 * a
