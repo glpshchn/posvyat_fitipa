9bb4aa878fedb40a7ae67ece2fe7868040349ef2
glpshchn <464976@niuitmo.ru>
2024-12-21 11:55:27 +0300

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,39 +1,35 @@
-import unittest
+import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from square import area, perimeter
 
+def test_negative_side_perimeter():
+    a = -13
+    with pytest.raises(ValueError):
+        perimeter(a)
 
-class TestSquare(unittest.TestCase):
+def test_negative_side_area():
+    a = -13
+    with pytest.raises(ValueError):
+        area(a)
 
-    def test1(self):
-        a = -13
-        with self.assertRaises(ValueError):
-            perimeter(a)
+def test_zero_side_area():
+    a = 0
+    correct = 0
+    assert area(a) == correct
 
-    def test2(self):
-        a = -13
-        with self.assertRaises(ValueError):
-            area(a)
+def test_zero_side_perimeter():
+    a = 0
+    correct = 0
+    assert perimeter(a) == correct
 
-    def test3(self):
-        a = 0
-        correct = 0
-        self.assertEqual(area(a), correct)
+def test_positive_side_area():
+    a = 13
+    correct = a * a
+    assert area(a) == correct
 
-    def test4(self):
-        a = 0
-        correct = 0
-        self.assertEqual(perimeter(a), correct)
-
-    def test5(self):
-        a = 13
-        correct = a * a
-        self.assertEqual(area(a), correct)
-
-    def test6(self):
-        a = 13
-        correct = 4 * a
-        self.assertEqual(perimeter(a), correct)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_positive_side_perimeter():
+    a = 13
+    correct = 4 * a
+    assert perimeter(a) == correct
